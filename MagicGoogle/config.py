760a6e60c3eec2d947852a112af8fc398fc3bb54
gbumarix <gbumarix@gmail.com>
2017-05-27 15:14:31 +0800

    add vid and safe search

--- MagicGoogle/config.py
+++ MagicGoogle/config.py
@@ -4,6 +4,6 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36
 BLACK_DOMAIN = ['www.google.gf', 'www.google.io','www.google.com.lc']
 
 DOMAIN = 'www.google.com'
-URL_SEARCH = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1"
-URL_NUM = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1&num={num}"
-URL_NEXT = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1&num={num}&start={start}"
+URL_SEARCH = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1&safe=inactive&tbm=vid"
+URL_NUM = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1&num={num}&safe=inactive&tbm=vid"
+URL_NEXT = "https://{domain}/search?hl={language}&q={query}&btnG=Search&gbv=1&num={num}&start={start}&safe=inactive&tbm=vid"
